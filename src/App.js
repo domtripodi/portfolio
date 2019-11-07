d60957d78ad41ed62378daeece34860afc482ded
Dom Trip <tridy018@mymail.unisa.edu.au>
2019-11-08 00:47:52 +1030

    Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import NavigationBar from './components/js/NavigationBar';
 import Gallery from './components/js/Gallery';
 import About from './components/js/About';
 import Contact from './components/js/Contact';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+// import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import { HashRouter, Route, Link, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <HashRouter basename="/">
         <NavigationBar/>
-          <Switch>
-            <Redirect from="/portfolio/" to="/portfolio/gallery" exact/>
-            <Route path="/portfolio/gallery" component={Gallery}/>
-            <Route path="/portfolio/about" component={About}/>
-            <Route path="/portfolio/contact" component={Contact}/>
-          </Switch>
-        </Router>
+          {/* <Switch> */}
+            <Redirect from='/' to="/gallery"  exact/>
+            <Route path="/gallery" component={Gallery}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          {/* </Switch> */}
+        </HashRouter>
       </div>
     );
   }
--- src/components/js/NavigationBar.js
+++ src/components/js/NavigationBar.js
@@ -8,15 +8,15 @@ class NavigationBar extends React.Component {
     render() {
         return (
             <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Game Portfolio</Navbar.Brand>
+                <Navbar.Brand href="#gallery">Game Portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <NavLink className="navBar-inactive navItemStyling" to="/portfolio/gallery">Gallery</NavLink>
-                        <NavLink className="navBar-inactive navItemStyling" to="/portfolio/about">About</NavLink>
+                        <NavLink className="navBar-inactive navItemStyling" to="/gallery">Gallery</NavLink>
+                        <NavLink className="navBar-inactive navItemStyling" to="/about">About</NavLink>
                     </Nav>
                     <Nav>
-                    <NavLink className="navBar-inactive navItemStyling" to="/portfolio/contact">Contact</NavLink>
+                    <NavLink className="navBar-inactive navItemStyling" to="/contact">Contact</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
